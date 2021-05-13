17f66286a1811fb0c25f11642ce64972399acebd
Rene Cabrera <renecabrera@outlook.com>
2021-05-12 21:37:38 -0400

    septimo commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("a").click(function(event){
+        // esta linea desactiva click por default
+        event.preventDefault();
+
+        var opcionMenu = this.hash;
+
+        $("html").animate(
+            {
+            scrollTop: $(opcionMenu).offset().top,
+        },
+        800
+        );
+
+    });
+
+});
